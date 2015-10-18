8d822448f8e03913d38e1c264e26616b82bc2e92
Diez B. Roggisch <deets@web.de>
2015-10-18 22:58:46 +0200

    Now new with ligthing, and a bit saner rotation mapping

--- visualization/static/js/visualization.js
+++ visualization/static/js/visualization.js
@@ -25,16 +25,30 @@ $(document).ready(function() {
     document.body.appendChild( renderer.domElement );
 
     var geometry = new THREE.BoxGeometry( 5, 1, 5 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
     var cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
+    var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+    scene.add( light );
 
-    camera.position.z = 5;
+    var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+    directionalLight.position.set( 0, 0, 10 );
+    scene.add( directionalLight );
+
+    camera.position.z = 15;
 
     var render = function () {
 	requestAnimationFrame( render );
 	renderer.render(scene, camera);
     };
+
+    var coordinateRemapMatrix = new THREE.Matrix4();
+    coordinateRemapMatrix.set(
+        0,  0,  1,  0,
+       -1,  0,  0,  0,
+        0, -1,  0,  0,
+        0,  0,  0,  1
+    );
 
     function rotateCube(message) {
         var input = JSON.parse(message);
@@ -44,6 +58,10 @@ $(document).ready(function() {
         q.x = attitude[1];
         q.y = attitude[2];
         q.z = attitude[3];
+        var m = new THREE.Matrix4();
+        m.makeRotationFromQuaternion(q);
+        m.multiply(coordinateRemapMatrix);
+        q.setFromRotationMatrix(m);
         cube.quaternion.copy(q);
     }
 
