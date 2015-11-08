19cf9b0cc50ac6a24533f2e039b48d9f80773f08
Diez B. Roggisch <deets@web.de>
2015-11-08 18:38:14 +0100

    Visualization is correct from MPU6050 quaternion - mostly
    For yet unknown reasons, it's only 50% of the rotation.

--- visualization/static/js/visualization.js
+++ visualization/static/js/visualization.js
@@ -30,15 +30,10 @@ function setupScene() {
     renderer.setSize(scene_root.width(), scene_root.height());
     scene_root.append(renderer.domElement);
 
-    var geometry = new THREE.BoxGeometry( 5, 1, 5 );
     var material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
     var light = new THREE.AmbientLight( 0x404040 ); // soft white light
-    scene.add( light );
-
     var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-    directionalLight.position.set( 0, 0, 10 );
+    directionalLight.position.set( 0, 50, 100 );
     scene.add( directionalLight );
 
     camera.position.z = 15;
@@ -49,7 +44,35 @@ function setupScene() {
 	renderer.render(scene, camera);
     };
 
+    var m1 = new THREE.Matrix4();
+    var mr1 = new THREE.Matrix4();
+    mr1.makeRotationZ(Math.PI / 2);
+    var mr2 = new THREE.Matrix4();
+    mr2.makeRotationX(Math.PI / 2);
+
+    m1.multiplyMatrices(mr1, mr2);
+
+    var m2 = new THREE.Matrix4();
+    m2.getInverse(m1);
+
+    var the_pi = null;
+    var loader = new THREE.OBJLoader();
+    // load a resource
+    loader.load(
+	// resource URL
+	'/static/rpi.obj',
+	// Function when resource is loaded
+	function ( object ) {
+            the_pi = object;
+            object.scale.multiplyScalar(.1);
+            scene.add(object);
+	}
+    );
+
     function rotateCube(input) {
+        if(the_pi === null) {
+            return;
+        }
         var q = new THREE.Quaternion();
         var attitude = input["attitude"];
         q.w = attitude[0];
@@ -57,13 +80,18 @@ function setupScene() {
         q.y = attitude[2];
         q.z = attitude[3];
         var m = new THREE.Matrix4();
-        m.makeRotationFromQuaternion(q);
+        var rs = new THREE.Matrix4();
+        var r = new THREE.Matrix4();
+        rs.makeRotationFromQuaternion(q);
+        r.getInverse(rs);
+        m.multiply(m2);
+        m.multiply(r);
+        m.multiply(m1);
         q.setFromRotationMatrix(m);
-        cube.quaternion.copy(q);
+        the_pi.quaternion.copy(q);
     }
 
     render();
-
     return rotateCube;
 }
 
