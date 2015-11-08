54862e0d03d35888c4a345a3e8f8e7a16d6325a3
Diez B. Roggisch <deets@web.de>
2015-11-08 17:21:15 +0100

    Simple FPS display

--- visualization/mpu6050-read.py
+++ visualization/mpu6050-read.py
@@ -2,6 +2,7 @@ import pprint
 import nanomsg
 import json
 import sys
+import time
 
 uri = "tcp://192.168.2.102:5555"
 
@@ -10,13 +11,18 @@ socket.connect(uri)
 
 key = sys.argv[1] if len(sys.argv) > 1 else None
 
+then = time.time()
 while True:
     m = socket.recv()
+    elapsed = time.time() - then
+    then += elapsed
+
     try:
         data = json.loads(m)
         if key:
             data = data[key]
         pprint.pprint(data)
-        print "#" * 40
+        print ("#" * 40) + " FPS: %.2f" % (1.0 / elapsed)
+
     except:
         print m
